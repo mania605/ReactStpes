c149c24946224e30904480a57298a6d01a3fbcc8
mania605 <kim.minsun83@gmail.com>
2024-10-01 11:17:49 +0900

    리액트 컴포넌트 안쪽의 JSX구문 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+// /* 컴포넌트 함수
+// JSX라는 리액트만의 문법체계(돔생성 스크립트 구문을 HTML형식을 본따 만든 템플릿)
+// 컴포넌트 함수 생성 규칙
+// 1.무조건 이름 대문자(대문자 아닌 소문자로 작성하면 일반 태그로 인식해서 오류발생)
+// 2. JSX반환
+// 3. export문으로 내보냄
+// 4. 하나의 컴포넌트 함수는 단일 요소만 반환 가능
+// 5. 결국은 자바스크립트이기 때문에 JS예약어는 사용 불가(class)
+// 6. 컴포넌트 호출은 먼저 import문으로 해당 함수명 가져온 뒤 호출
+// 7. 컴포넌트 호출방식<컴포넌트 함수명/>
+// 8 .만약 불필요한 Wrapping요소 없이 그룹화 하고 싶을 때 fragment<></> 형태로 감싸줌
+// */
+
+
 function App() {
 	return (
-		<div>
-			<h1 className='abc' style={{ color: 'red' }}>
-				Hello
-			</h1>
-		</div>
+<>
+			<h1 className="abc">Hello</h1>
+			<ul>
+				<li>list1</li>
+				<li>list2</li>
+				<li>list3</li>
+			</ul>
+</>
 	);
 }
 
+//작업 내보내기 가져오기import 해야 됨.
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -7,4 +7,5 @@ import App from "./App.jsx";
 //루트 컴포넌트에 적용하 ㄹ전체 스타일 파일
 import "./styles/index.css";
 
+//App이라는 마스터 컴포넌트를 돔 형태로 구현해서 index.html의 #root요소안에 넣어줌.
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
