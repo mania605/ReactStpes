79e553e77f8b9725dc0f4356df976af97b140b8e
mania605 <kim.minsun83@gmail.com>
2024-10-01 14:43:46 +0900

    Footer 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+import Home from './components/main/Home';
 export default function App() {
 	return (
 		<>
-<h1 className="tit">Title</h1>		 
+			<Header />
+			<Home />
+			<Footer />
 		</>
 	);
-} 
+}
