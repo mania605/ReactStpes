9099710b89fef284c6c8ef2a39a09b0a4efa8ef6
mania605 <kim.minsun83@gmail.com>
2024-10-02 09:40:05 +0900

    color를 props로 처리 페이지마다 다른색적용

--- src/components/sub/Members.jsx
+++ src/components/sub/Members.jsx
@@ -3,7 +3,7 @@ import Title from '../common/Title';
 export default function Members() {
 	return (
 		<main className='members'>
-			<Title />
+			<Title text={'MEMEBRS'} color={'red'} />
 		</main>
 	);
 }
